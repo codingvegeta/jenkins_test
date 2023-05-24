9eb341972e1cc5fd964abd3356698293e7bc5a62
codingvegeta <hong090992@gmail.com>
2023-05-24 13:01:10 +0900

    jenkins test 중

---
+++ src/main/java/com/example/hi/HiController.java
@@ -0,0 +1,12 @@
+package com.example.hi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+    @GetMapping("/")
+    public String hi() {
+        return "Hello World";
+    }
+}
