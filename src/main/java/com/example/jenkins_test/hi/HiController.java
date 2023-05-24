24b48a34f9070cfd38a30804f5d29dbae4cfd18a
codingvegeta <hong090992@gmail.com>
2023-05-24 14:44:12 +0900

    jenkins test 중

--- src/main/java/com/example/jenkins_test/hi/HiController.java
+++ src/main/java/com/example/jenkins_test/hi/HiController.java
@@ -1,4 +1,4 @@
-package com.example.hi;
+package com.example.jenkins_test.hi;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
